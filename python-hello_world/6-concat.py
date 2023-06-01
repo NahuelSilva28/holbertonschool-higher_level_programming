87573cc78e8fed158c39d57956f7570656f5dac2
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-06-01 13:53:20 -0700

    xd

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome"
-str2 = "to "
-str3 = "Holberton School!"
-print(str1 + " " + str2 + str3)
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print("Welcome to {}!".format(str1))
