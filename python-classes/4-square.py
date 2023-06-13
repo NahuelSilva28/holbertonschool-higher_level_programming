869c9a695dc8618d79481bce8e3baa4b6ebd0b52
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-06-13 20:48:37 -0300

    square 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 3-square.py)"""
+
+
+class Square:
+    """class Square"""
+    def __init__(self, size=0):
+        """new instantance of square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """get the size of the square"""
+        return self.__size
+ 
+    @size.setter
+    def size(self, value):
+        """set the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+ 
+    def area(self):
+        """calcula area of the square"""
+        return self.__size ** 2
+ 
