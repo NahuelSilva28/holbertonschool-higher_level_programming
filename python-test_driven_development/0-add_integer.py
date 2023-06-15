fd01bb65d0d8cddf670baf5e91a4e9a92804dbd0
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-06-15 18:19:25 -0300

    0 task

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,3 +2,27 @@
 """Write a function that adds 2 integers"""
 
 
+def add_integer(a, b=98):
+    """_summary_
+
+    Args:
+        a (int or float): The first number to be added.
+        b (int or float, optional): The second number to be added.
+            Defaults to 98.
+
+    Raises:
+        TypeError: If `a` is not an integer or float.
+        TypeError: If `b` is not an integer or float.
+
+    Returns:
+        int: The addition of `a` and `b`, casted to an integer.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
