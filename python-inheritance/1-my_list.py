eb205d9656150f1c36bcca108bbff821df1eb273
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-06-21 15:26:16 -0300

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,8 @@ class MyList(list):
     def print_sorted(self):
         """Write a class MyList that inherits from list:
         """
+        if not all(isinstance(x, int) for x in self):
+            raise TypeError("Only integers")
+        
         sorted_list = sorted(self)
         print(sorted_list)
