61db767457b54fff13a1a71021411d8a1e17c891
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-06-22 03:45:58 -0300

    pycode

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -3,9 +3,8 @@
 (based on 5-base_geometry.py)."""
 
 class BaseGeometry:
+    """Calculate the area of a geometric shape.
+    Raises:
+    Exception: This method is not implemented in the base class."""
     def area(self):
-        """Calculate the area of a geometric shape.
-
-        Raises:
-        Exception: This method is not implemented in the base class."""
         raise Exception("area() is not implemented")
