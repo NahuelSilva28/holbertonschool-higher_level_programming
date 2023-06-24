d4862278fe65a7f4060d1a76c8238b0afe1a2338
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-06-24 18:05:27 -0300

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""
-Write a script that adds all arguments
-to a Python list, and then save them to a file:
+"""Write a script that adds all arguments to a Python list, and then save them to a file:
 """
 
 
 import sys
 
 
-# Importing functions from external modules
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-
 try:
-    # Load existing data from the file
-    data = load_from_json_file(filename)
-except FileNotFoundError:
-    # Create an empty list if the file doesn't exist
-    data = []
-
-# Add command line arguments to the list
-data.extend(sys.argv[1:])
-
-# Save the updated data to the file
-save_to_json_file(data, filename)
+    list = load_from_json_file(filename)
+    for i in range(1, len(sys.argv)):
+        list.append(sys.argv[i]), save_to_json_file(list, filename)
+except Exception:
+    list = []
+    for i in range(1, len(sys.argv)):
+        list.append(sys.argv[i]), save_to_json_file(list, filename)
+    
