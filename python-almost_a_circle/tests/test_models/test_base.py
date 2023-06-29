ff2d0a3cf7c205e77b1eba5a7d66f186144122a1
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-06-29 15:40:17 -0300

    a

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,16 +1,4 @@
 import unittest
-from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
-
-class Test_Base(unittest.TestCase):
-    def test_2(self):
-        a = Base()
-        self.assertEqual(a.id, 1)
-        b = Base(5)
-        self.assertEqual(b.id, 4)
-
-
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
