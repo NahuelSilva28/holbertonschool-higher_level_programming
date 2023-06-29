1ac74d0d8953713004222f443bea80bfb3ef65d4
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-06-29 16:59:55 -0300

    rec

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Rectangle class from base"""
 
-from .base import Base
+from models.base import Base
+
 
 class Rectangle(Base):
     """Rectangle class"""
@@ -9,74 +10,58 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor method"""
         super().__init__(id)
-        self._width = width
-        self._height = height
-        self._x = x
-        self._y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
         """Getter method for width"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """Setter method for width"""
-        if not isinstance(value, int):
-            raise TypeError("Width must be an integer")
-        if value <= 0:
-            raise ValueError("Width must be > 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
         """Getter method for height"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """Setter method for height"""
-        if not isinstance(value, int):
-            raise TypeError("Height must be an integer")
-        if value <= 0:
-            raise ValueError("Height must be > 0")
-        self._height = value
+        self.__height = value
 
     @property
     def x(self):
         """Getter method for x"""
-        return self._x
+        return self.__x
 
     @x.setter
     def x(self, value):
         """Setter method for x"""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self._x = value
+        self.__x = value
 
     @property
     def y(self):
         """Getter method for y"""
-        return self._y
+        return self.__y
 
     @y.setter
     def y(self, value):
         """Setter method for y"""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self._y = value
+        self.__y = value
 
     def area(self):
         """Calculates and returns the area of the rectangle"""
-        return self._width * self._height
+        return self.__width * self.__height
 
     def display(self):
         """Prints the rectangle using '#' character"""
-        rectangle = "\n" * self._y + (" " * self._x + "#" * self._width + "\n") * self._height
+        rectangle = "\n" * self.__y + (" " * self.__x + "#" * self.__width + "\n") * self.__height
         print(rectangle, end="")
 
     def update(self, *args, **kwargs):
@@ -95,12 +80,12 @@ class Rectangle(Base):
         """Returns the dictionary representation of the rectangle"""
         return {
             'id': self.id,
-            'width': self._width,
-            'height': self._height,
-            'x': self._x,
-            'y': self._y
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y
         }
 
     def __str__(self):
         """String representation of the rectangle"""
-        return f"[Rectangle] ({self.id}) {self._x}/{self._y} - {self._width}/{self._height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
