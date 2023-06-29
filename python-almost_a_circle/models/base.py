cb9e39c80c6e3507d9fd4812a9342442aba64579
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-06-29 15:54:06 -0300

    aaaaaa

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 
 
 import json
+from venv import create
+
 
 
 class Base:
--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,14 @@
 import unittest
+from base import Base
+
+
+class TestBase(unittest.TestCase):
+    def test_2(self):
+        a = Base()
+        self.assertEqual(a.id, 1)
+        b = Base(5)
+        self.assertEqual(b.id, 5)
+
 
 if __name__ == '__main__':
     unittest.main()
