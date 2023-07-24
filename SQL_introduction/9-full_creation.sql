c88c4819c4013f9edd3114ec8ef7c42736e8d0f7
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-07-24 16:58:51 -0700

    9creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- 9. Full creation
+-- Create the second_table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+-- Insert
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10),(2, 'Alex', 3),(3, 'Bob', 14),(4, 'George', 8);
