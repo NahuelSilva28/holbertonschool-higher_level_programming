834f0c49cfbbaff7140dbcfda2f77217f0355eee
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-07-26 14:16:47 -0700

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- 9. Cities by States
 -- lists all cities
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = states.id
+SELECT cities.id, cities.name, states.name
+FROM cities INNER JOIN states
+ON cities.state_id = states.id
 ORDER BY cities.id ASC;
 
